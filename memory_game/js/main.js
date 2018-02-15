1821b49ce8e86543036c29f69de6b31318c5c178
Mandy <mandy.tang@me.com>
2018-02-15 22:03:35 +1100

    created game  logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,16 @@
-var cardOne = "queen";
-var CardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
 
-console.log("User Flipped " + cardOne);
-console.log("User Flipped " + cardThree);
+	console.log("User flipped queen");
+	console.log("User flipped king");
+
+cardsInPlay.push('cardOne');
+cardsInPlay.push('cardTwo');
+
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+alert("You found a match!");
+} else {
+alert("Sorry, try again.");	
+}
